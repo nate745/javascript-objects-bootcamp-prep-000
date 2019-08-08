4c9eb14eb84a8d214cf961f1ed9d60b354741ee7
Nathan Alex <nate745@gmail.com>
2019-08-08 09:18:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {Kendrick : "Dying of Thirst"}
+var playlist = {"Phil Ochs" : "Dying of Thirst"}
 
 function updatePlaylist(playlist,artistName,songTitle){
   return Object.assign({}, playlist, {["My Bloody Valentine"]:"Phil Ochs"})
